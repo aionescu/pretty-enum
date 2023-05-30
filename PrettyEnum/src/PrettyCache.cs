02834ee48b4acdf9102a301e82abec55a8041107
Alex Ionescu <aaionescu@pm.me>
2023-05-30 22:16:28 +0200

    Move misc methods into `PrettyCache`

--- PrettyEnum/src/PrettyCache.cs
+++ PrettyEnum/src/PrettyCache.cs
@@ -1,11 +1,13 @@
 namespace PrettyEnum;
 
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 static class PrettyCache<TEnum> where TEnum : struct, Enum {
-  internal static readonly bool hasIgnoreAttribute;
-  internal static readonly bool hasFlagsAttribute;
+  internal static readonly bool hasIgnoreAttribute, hasFlagsAttribute;
 
   internal static readonly TEnum[] enumValues;
   internal static readonly string[] enumPrettyNames;
@@ -17,8 +19,8 @@ static class PrettyCache<TEnum> where TEnum : struct, Enum {
   internal static readonly Func<TEnum, TEnum, bool>? and;
 
   static PrettyCache() {
-    hasIgnoreAttribute = typeof(TEnum).HasAttribute<IgnorePrettyPrintAttribute>(out _);
-    hasFlagsAttribute = typeof(TEnum).HasAttribute<FlagsAttribute>(out _);
+    hasIgnoreAttribute = HasAttribute<IgnorePrettyPrintAttribute>(typeof(TEnum), out _);
+    hasFlagsAttribute = HasAttribute<FlagsAttribute>(typeof(TEnum), out _);
 
     enumValues = (Enum.GetValues(typeof(TEnum)) as TEnum[])!;
     var enumValueNames = Enum.GetNames(typeof(TEnum));
@@ -61,31 +63,43 @@ static class PrettyCache<TEnum> where TEnum : struct, Enum {
     }
   }
 
-  private static string Capitalize(string s)
-    => char.ToUpperInvariant(s[0]) + s.Substring(1).ToLowerInvariant();
+  private static bool HasAttribute<TAttribute>(MemberInfo memberInfo, [NotNullWhen(true)] out TAttribute? attribute) where TAttribute : Attribute {
+    var attrs = (memberInfo.GetCustomAttributes(typeof(TAttribute), false) as TAttribute[])!;
+
+    if (attrs.Length > 0) {
+      attribute = attrs[0];
+      return true;
+    } else {
+      attribute = default;
+      return false;
+    }
+  }
 
   private static string FromCamelCase(string s) {
-    var replaced =
-      s
-      .RegexReplace("([A-Z])([a-z])", " $1$2")
-      .RegexReplace("([a-z])([A-Z])", "$1 $2")
-      .RegexReplace("([0-9])([a-zA-Z])", " $1$2")
-      .RegexReplace("([a-zA-Z])([0-9])", "$1 $2");
+    var ro = RegexOptions.Compiled | RegexOptions.CultureInvariant;
 
-    var words = replaced.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(Capitalize);
+    s = Regex.Replace(s, "([A-Z])([a-z])", " $1$2", ro);
+    s = Regex.Replace(s, "([a-z])([A-Z])", "$1 $2", ro);
+    s = Regex.Replace(s, "([0-9])([a-zA-Z])", " $1$2", ro);
+    s = Regex.Replace(s, "([a-zA-Z])([0-9])", "$1 $2", ro);
+
+    var words =
+      s.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+      .Select(w => char.ToUpperInvariant(w[0]) + w.Substring(1).ToLowerInvariant());
+
     return string.Join(" ", words);
   }
 
   private static string PrettyPrintSingleValue(string rawName) {
     var field = typeof(TEnum).GetField(rawName)!;
 
-    if (field.HasAttribute<IgnorePrettyPrintAttribute>(out _))
+    if (HasAttribute<IgnorePrettyPrintAttribute>(field, out _))
       return rawName;
 
-    if (field.HasAttribute<PrettyNameAttribute>(out var attr) && !string.IsNullOrWhiteSpace(attr.PrettyName))
+    if (HasAttribute<PrettyNameAttribute>(field, out var attr) && !string.IsNullOrWhiteSpace(attr.PrettyName))
       return attr.PrettyName;
 
-    if (field.HasAttribute<DescriptionAttribute>(out var descAttr) && !string.IsNullOrWhiteSpace(descAttr.Description))
+    if (HasAttribute<DescriptionAttribute>(field, out var descAttr) && !string.IsNullOrWhiteSpace(descAttr.Description))
       return descAttr.Description;
 
     var words = rawName.Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries).Select(FromCamelCase);
